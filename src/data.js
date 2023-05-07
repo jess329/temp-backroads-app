6b080f2f308170cb01f17a51e176d8b96c8a3740
jasper <jasper.mittag@outlook.de>
2023-05-07 16:26:56 +0200

    links update

--- src/data.js
+++ src/data.js
@@ -14,9 +14,9 @@ export const pageLinks = [
     {id: 4, href: "#contact", text: "contact", class: "nav-link"}
 ]
 export const socialLinks = [
-    {id: 1, href: "https://www.twitter.com", classLink: "nav-icon", classIcon: "fab fa-facebook"},
+    {id: 1, href: "https://www.facebook.com", classLink: "nav-icon", classIcon: "fab fa-facebook"},
     {id: 2, href: "https://www.twitter.com", classLink: "nav-icon", classIcon: "fab fa-twitter"},
-    {id: 3, href: "https://www.twitter.com", classLink: "nav-icon", classIcon: "fab fa-squarespace"},
+    {id: 3, href: "https://www.squarespace.com", classLink: "nav-icon", classIcon: "fab fa-squarespace"},
 ]
 export const servicesData = [
     {id: 1, icon: "fas fa-wallet fa-fw",title: "saving money", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia"},
